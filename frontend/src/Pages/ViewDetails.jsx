a324f3c85a6ed6fb0ae6a02c5801f4328b8ce76e
Ujjwal Awasthi <ujjawasthi@gmail.com>
2024-11-29 13:44:40 +0530

    new update

--- frontend/src/Pages/ViewDetails.jsx
+++ frontend/src/Pages/ViewDetails.jsx
@@ -14,7 +14,7 @@ const ViewDetails = () => {
   <img src={location.state.image} className="card-img-top " alt="..." />
   <div className="card-body">
     <h5 className="card-title"><b>Title:</b>{location.state.title}</h5>
-    <p className="card-text">Description: {location.state.description}</p>
+    <p className="card-text">Description: {location.state.content}</p>
     {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
    
   </div>
