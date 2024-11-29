45578e31ef8d10defd0f24645b4e0c5241933ec3
Ujjwal Awasthi <ujjawasthi@gmail.com>
2024-11-29 13:43:38 +0530

    updated description

--- frontend/src/Pages/ViewDetails.jsx
+++ frontend/src/Pages/ViewDetails.jsx
@@ -14,7 +14,7 @@ const ViewDetails = () => {
   <img src={location.state.image} className="card-img-top " alt="..." />
   <div className="card-body">
     <h5 className="card-title"><b>Title:</b>{location.state.title}</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p className="card-text">Description: {location.state.description}</p>
     {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
    
   </div>
