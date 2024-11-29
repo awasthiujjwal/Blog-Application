b8568f8a0cffe380e9b9346a3767123ff05e5d44
Ujjwal Awasthi <ujjawasthi@gmail.com>
2024-11-29 12:25:41 +0530

    updated

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require ('mongoose')
+require('dotenv').config()
 const connectToDb=async()=>{
-   await mongoose.connect('mongodb://127.0.0.1:27017/test')
+   await mongoose.connect(`mongodb+srv://${process.env.mongousername}:${process.env.mongopassword}@blogapplication.f5xgl.mongodb.net/?retryWrites=true&w=majority&appName=Blogapplication`)
     .then(()=>console.log("successfully connected to db"))
     .catch(()=>console.log("did not connect to db",))
 }
